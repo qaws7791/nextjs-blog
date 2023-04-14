1de2cf34e14d94af8f51cba249e2c1002cd2759e
qaws7791 <qaws7791@gmail.com>
2023-04-15 00:28:33 +0900

    home 페이지 타이틀 추가

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import {sanityClient} from '@/lib/sanity.server'
 import PostPreview from '@/components/post-preview'
 import {recentPostQuery} from '@/lib/queries'
+import styles from './page.module.css'
 
 export default async function Home() {
   const posts = await getStaticData()
   return (
     <>
+      <h1 className={styles.title}>Recently Post</h1>
       {posts.map((post) => (
         <PostPreview post={post} key={post.title} />
       ))}
